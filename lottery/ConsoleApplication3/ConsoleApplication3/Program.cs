0381d2ce58af2fa588efdb7d595fa256c2e9e123
m305-40\w401 <oi455@er555.fdrg>
2018-03-22 17:59:50 +0800

    重傳一次

--- lottery/ConsoleApplication3/ConsoleApplication3/Program.cs
+++ lottery/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -17,12 +17,13 @@ namespace ConsoleApplication3
             string[] names = { "葉俊廷", "張澤瑜", "王程捷", "江儀婷", "張秉華", "陳柏霖", "黃紀瑜", "黃昱維", "蔡逸群", "登琳", "世新魏鈞孝吧", "劉定南", "陳信如", "童信傑", "李岳倫", "鄒和恆", "簡毓玟", "劉子瑄", "蕭紹洋", "李亞宸la", "張以潔", "邱仕紳", "呂家瑩", "世新林鑫佑", "徐均得", "陳佳欣", "張恩瑋", "周詮", "王湘婷", "蕭宇成", "Max Cheung(BANG)", "馬嘉誠Σヽ(ﾟД ﾟ; )ﾉ魔貫光殺砲(ﾟДﾟ)σ━0000", "陳昱嘉", "康珈熏", "盧奕宏", "陳郁佳的大哥黃博涵", "遲正雯", "世新林星彤", "許晏誠ｘ勒是奇多＿８＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝Ｄ　", "許子安", "高子晴", "陳思婷", "矝鵘鮿", "余彥廷", "吳耀輝", "鄭丞智", "林冠廷", "世新何曜宇", "邵喬雨", "張豐愷", "林子晴", "邱雁回", "陳家欣是韓妞", "鄧雅馨", "謝汶珊", "李曼寧", "世新徐偉哲", "张钰慈", "鍾宜珊", "raer_tsai", "世新李姳諼", "鄭曼君", "陳柏霖", "世新許子安", "世新方若帆" };
 
             // 隨機產生序號
-            Random rand = new Random();
-            int r = rand.Next(0, heights.Length);
-
+            //Random rand = new Random();
+            //int r = rand.Next(0, heights.Length);
+               
             // 顯示
+            for(int r = 0; r < heights.Length; r++)
             Console.WriteLine(r + ":" + heights[r]);
-            Console.WriteLine(r + ":" + names[r]);
+            //Console.WriteLine(r + ":" + names[r]);
 
             // 停止螢幕
             Console.ReadLine();
